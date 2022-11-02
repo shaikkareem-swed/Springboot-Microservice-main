2cf6c74ba8fb9ce0fcd8b469b801bbbce1fa71e6
Shaik Kareemulla <86288284+shaikkareem-swed@users.noreply.github.com>
2022-11-02 12:03:45 +0100

    Update CloudConfigServerApplication.java

--- Springboot-Microservice-main/Springboot-Microservice-main/cloud-config-server/src/main/java/com/dailycodebuffer/cloud/CloudConfigServerApplication.java
+++ Springboot-Microservice-main/Springboot-Microservice-main/cloud-config-server/src/main/java/com/dailycodebuffer/cloud/CloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.dailycodebuffer.cloud;
+package com.cloud.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
